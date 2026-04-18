695a76184dc18fe2e0f8730f9eb4f8f564e67638
kartikeyjaina <149296478+kartikeyjaina@users.noreply.github.com>
2026-04-18 11:49:08 +0530

    Remove unused Github import from App.jsx
    Removed unused import for Github component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import "./app.scss";
 import Dock from "./components/Dock";
 import Nav from "./components/Nav";
-import Github from "./components/windows/github";
 import Note from "./components/windows/Note";
 import Resume from "./components/windows/Resume";
 import Spotify from "./components/windows/Spotify";
